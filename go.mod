a1973cf9f0d19beb8cf53406849146a87b2a7798
7onetella <7onetella@gmail.com>
2019-11-13 16:41:46 -0500

    updating

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/7onetella/webapp
 
 go 1.12
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/go-redis/redis/v7 v7.0.0-beta.4
+	github.com/gorilla/mux v1.7.3
+)
--- main.go
+++ main.go
@@ -7,8 +7,15 @@ import (
 	"os"
 	"time"
 
+	"github.com/go-redis/redis/v7"
 	"github.com/gorilla/mux"
 )
+
+var client *redis.Client
+
+func init() {
+	NewRedisClient()
+}
 
 func main() {
 	port := os.Getenv("HTTP_PORT")
@@ -16,6 +23,8 @@ func main() {
 	r := mux.NewRouter()
 	r.Path("/api/health").Methods("GET").HandlerFunc(HealthCheckHandler)
 	r.Path("/api/hello").Methods("GET").HandlerFunc(HelloHandler)
+	r.Path("/redis/get/{key}").Methods("GET").HandlerFunc(RedisGetHandler)
+	r.Path("/redis/set/{key}/{value}").Methods("GET").HandlerFunc(RedisSetHandler)
 	http.Handle("/", r)
 
 	srv := &http.Server{
@@ -47,4 +56,49 @@ func HelloHandler(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("HELLO"))
+}
+
+// NewRedisClient new redis client
+func NewRedisClient() {
+	inst := redis.NewClient(&redis.Options{
+		Addr:     "dell-vm7.7onetella.net:22221",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := inst.Ping().Result()
+	fmt.Println(pong, err)
+	// Output: PONG <nil>
+	if err == nil {
+		client = inst
+	}
+}
+
+// RedisGetHandler redis get calls
+func RedisGetHandler(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	key := params["key"]
+	fmt.Println("key=", key)
+
+	val, err := client.Get(key).Result()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(val))
+}
+
+// RedisSetHandler redis set call
+func RedisSetHandler(w http.ResponseWriter, req *http.Request) {
+	params := mux.Vars(req)
+	key := params["key"]
+	value := params["value"]
+
+	err := client.Set(key, value, 0).Err()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 }
